9fc6c603fee9fee5e5b010c34478d3dec9ba11fa
GhHiva <108700626+GhHiva@users.noreply.github.com>
2025-04-29 14:51:57 -0600

    Add files via upload

---
+++ Module.py
@@ -0,0 +1,96 @@
+import math
+
+class Value:
+    def __init__(self, data, _children=(), _op='', label=''):
+        self.data = data
+        self.grad=0.0 # initialize the gradient
+        self._op= _op #show the operation in the graph
+        self._prev= set(_children) # _name: is for keeping the result and updating based on the previous step.
+        self._backward = lambda: None
+        self.label = label  # here we wanna labeling the numbers whoes we work on
+
+    def __repr__(self): # represent the output readable
+        return(f'Value(data={self.data})')
+
+    def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data + other.data, (self,other), '+')
+        def _backward():
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
+        out._backward = _backward
+        #print(f'This is addition : {out}')
+        return out #return the output as the Value not just the plain number # It helps to just show the result without None statement
+
+    def __neg__(self): # without this the code does not underestand the -2! I think since the sub works on the right side not left! in the case we just have one number!
+        return (self.data) * -1
+
+
+    def __sub__(self, other):
+        out = self.data + ( - other.data )
+        #print(f'This is sub : {out}')
+        return Value(out)
+
+    def __mul__(self, other):
+        other = other if isinstance (other, Value) else Value(other)
+        out = Value (self.data * other.data, (self, other), '*')
+        def _backward():
+            self.grad += out.grad * other.data
+            other.grad += out.grad * self.data
+        out._backward= _backward
+        #print(f'This is mul : {out}')
+        return out
+
+    def __rmul__(self, other): # commuting the product. Multiplication does not always commute! Like Matrix!
+        return self * other
+
+
+    def __truediv__(self, other): #python 3 use __truediv__ instead of __div__
+        out = self.data / other.data
+        # print(f'This is div : {out}')
+        return Value(out) # we can just use the return
+
+    def __pow__(self, other):
+        out = Value (self.data ** other.data, (self,), f'^ {other}')
+        #print(f'This is pow : {out}')
+        def _backwrd():
+            self.grad += out.grad * (other.data)* (self.data ** (other.data -1))
+        out._backward = _backward
+        return out
+
+    def exp(self):
+        x = self.data
+        out = Value (math.exp(x), (self,), 'exp')
+        def _backward():
+          self.grad += out.grad * out.data
+        out._backward = _backward
+        return out
+
+
+    def tanh(self):
+        x = self.data
+        t = (1-math.exp(2*x))/(1+math.exp(2*x))
+        out = Value(t, (self,), 'tanh') # (self,) is a child of tanh
+        def _backward():
+            self.grad += out.grad * (1-t**2)
+        out._backward = _backward # here we save function we do not want to execute it now so we write it without () !
+
+        return out
+
+
+    def backward(self):
+        topo=[]
+        visited=set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+        self.grad=1
+        for node in reversed(topo):
+            node._backward()
+
+if __name__=='__main__':
+    print('Yes')
